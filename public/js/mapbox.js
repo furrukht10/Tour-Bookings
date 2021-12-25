221550c33d1164f5bcf906890f4b42c0391e2cf0
furrukht10 <furrukht10@gmail.com>
2021-12-25 18:58:02 -0500

    Upload Files

---
+++ public/js/mapbox.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+export const displayMap = locations => {
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoiZnRyZWFwZXIxMCIsImEiOiJja3hsNTE4NGYzNmIyMnVtZmxzMG5rNGh4In0.V17Cgil6YgR_dU3rSzqm5w';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/ftreaper10/ckxl5f81k2xxn15rjdzd9o7m7',
+    scrollZoom: false
+    //   center: [-118.113491, 34.111745],
+    //   zoom: 10,
+    //   interactive: false,
+  });
+
+  const bounds = new mapboxgl.LngLatBounds();
+
+  locations.forEach(loc => {
+    // Create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
+
+    // Add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom'
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
+
+    // Add popup
+    new mapboxgl.Popup({
+      offset: 30
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+      .addTo(map);
+
+    // Extend map bounds to include current location
+    bounds.extend(loc.coordinates);
+  });
+
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100
+    }
+  });
+};
